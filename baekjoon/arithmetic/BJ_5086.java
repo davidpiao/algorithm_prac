d04faac4d2bbf74f2aad2af2ebfae8c254d40447
davidpiao <churry9797@gmail.com>
2023-07-27 16:02:31 +0900

    [BJ_2501] 약수 구하기

--- baekjoon/arithmetic/BJ_5086.java
+++ baekjoon/arithmetic/BJ_5086.java
@@ -1,31 +1,28 @@
 package arithmetic;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class BJ_5086 {
-
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         while(true) {
-
-            int first = in.nextInt();
-            int second = in.nextInt();
+            int first = sc.nextInt();
+            int second = sc.nextInt();
 
             if(first == 0 && second == 0) break;
-
             if(second % first == 0) {
                 System.out.println("factor");
             }
             else if(first % second == 0) {
                 System.out.println("multiple");
             }
+            else if(first == 0) {
+                System.out.println("infinity");;
+            }
             else {
                 System.out.println("neither");
             }
         }
-
     }
-
 }
