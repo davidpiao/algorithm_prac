a4318dbd2f798f92d23af082901894fdfd799532
davidpiao <churry9797@gmail.com>
2023-09-08 17:27:36 +0900

    [ãProg] 성격 유형 검사하기

---
+++ prog/etc/성격유형검사하기.java
@@ -0,0 +1,13 @@
+//title: 성격 유형 검사하기
+//link: https://school.programmers.co.kr/learn/courses/30/lessons/118666
+package etc;
+
+class 성격유형검사하기 {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+}
