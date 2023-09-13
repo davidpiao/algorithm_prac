101aa6fa280575ff7c009f49194037b4d3511ba8
davidpiao <churry9797@gmail.com>
2023-09-13 17:30:59 +0900

    [Prog] 폰캣몬

---
+++ prog/lv1/폰캣몬.java
@@ -0,0 +1,10 @@
+//title: 폰캣몬
+//link - https://school.programmers.co.kr/learn/courses/30/lessons/1845
+package lv1;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+}
